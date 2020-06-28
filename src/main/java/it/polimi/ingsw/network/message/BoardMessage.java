401d3b02cd493ed1bd17c646f4ee5b18a0dbd291
samuelkala <61750389+samuelkala@users.noreply.github.com>
2020-06-28 14:35:55 +0200

    add javadoc

--- src/main/java/it/polimi/ingsw/network/message/BoardMessage.java
+++ src/main/java/it/polimi/ingsw/network/message/BoardMessage.java
@@ -4,17 +4,29 @@ import it.polimi.ingsw.model.board.ReducedSpace;
 
 import java.util.Arrays;
 
+/**
+ * The message which contains the board. Used to send the updated board to the client.
+ */
 public class BoardMessage extends Message {
     private static final long serialVersionUID = -8014575220371739730L;
     private final ReducedSpace[][] board;
 
-
+    /**
+     * Default constructor.
+     *
+     * @param board The board to wrapped in the message.
+     */
     public BoardMessage(String nickname, MessageType messageType, ReducedSpace[][] board) {
         super(nickname, messageType);
         this.board = board;
 
     }
 
+    /**
+     * Returns the board contained in the message.
+     *
+     * @return a matrix of {@link it.polimi.ingsw.model.board.ReducedSpace}.
+     */
     public ReducedSpace[][] getBoard() {
         return board;
     }
