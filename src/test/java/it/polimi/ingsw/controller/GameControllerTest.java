1f61452aa4246639d1b41c54e843c57bad8cb399
Andrea Lanzi <andrea1.lanzi@mail.polimi.it>
2020-07-03 11:07:14 +0200

    Graceful endgame V2

--- src/test/java/it/polimi/ingsw/controller/GameControllerTest.java
+++ src/test/java/it/polimi/ingsw/controller/GameControllerTest.java
@@ -346,7 +346,7 @@ public class GameControllerTest {
         p1_apply = new PositionMessage(p1, MessageType.APPLY_EFFECT, List.of(new Position(1, 2)));
         gameController.onMessageReceived(p1_apply);
 
-        assertEquals(true, gameController.isGameFinished());
+        assertNull(Game.getInstance().getPlayerByNickname("AndreaLanzi"));
 
     }
 
